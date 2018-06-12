1314adc740952a980c9a04d7ebb6ba1b2f6e4f1c
张宏伟 <289381152@qq.com>
2018-06-12 11:15:55 +0800

    #01 工具类
    添加工具类

---
+++ app/src/main/java/com/demo/xbdz/myapplication/util/AppUtil.java
@@ -0,0 +1,327 @@
+package com.demo.xbdz.myapplication.util;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+
+/**
+ * @author Zhanghongwei
+ * 描述：应用工具类.
+ */
+
+public class AppUtil {
+
+    /**
+     * 读取application 节点  meta-data 信息
+     */
+    public static String readMetaDataFromApplication(Context context, String key) {
+        try {
+            ApplicationInfo appInfo = context.getPackageManager()
+                    .getApplicationInfo(context.getPackageName(),
+                            PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString(key);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 描述：打开并安装文件.
+     *
+     * @param context the context
+     * @param file    apk文件路径
+     */
+    public static void installApk(Context context, File file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file),
+                "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    /**
+     * 描述：卸载程序.
+     *
+     * @param context     the context
+     * @param packageName 包名
+     */
+    public static void uninstallApk(Context context, String packageName) {
+        Intent intent = new Intent(Intent.ACTION_DELETE);
+        Uri packageURI = Uri.parse("package:" + packageName);
+        intent.setData(packageURI);
+        context.startActivity(intent);
+    }
+
+
+    /**
+     * 用来判断服务是否运行.
+     *
+     * @param ctx       the ctx
+     * @param className 判断的服务名字 "com.xxx.xx..XXXService"
+     * @return true 在运行 false 不在运行
+     */
+    public static boolean isServiceRunning(Context ctx, String className) {
+        boolean isRunning = false;
+        ActivityManager activityManager = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> servicesList = activityManager.getRunningServices(Integer.MAX_VALUE);
+        Iterator<ActivityManager.RunningServiceInfo> l = servicesList.iterator();
+        while (l.hasNext()) {
+            ActivityManager.RunningServiceInfo si = (ActivityManager.RunningServiceInfo) l.next();
+            if (className.equals(si.service.getClassName())) {
+                isRunning = true;
+            }
+        }
+        return isRunning;
+    }
+
+    /**
+     * 停止服务.
+     *
+     * @param ctx       the ctx
+     * @param className the class name
+     * @return true, if successful
+     */
+    public static boolean stopRunningService(Context ctx, String className) {
+        Intent intent_service = null;
+        boolean ret = false;
+        try {
+            intent_service = new Intent(ctx, Class.forName(className));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (intent_service != null) {
+            ret = ctx.stopService(intent_service);
+        }
+        return ret;
+    }
+
+
+    /**
+     * Gets the number of cores available in this device, across all processors.
+     * Requires: Ability to peruse the filesystem at "/sys/devices/system/cpu"
+     *
+     * @return The number of cores, or 1 if failed to get result
+     */
+    public static int getNumCores() {
+        try {
+            //Get directory containing CPU info
+            File dir = new File("/sys/devices/system/cpu/");
+            //Filter to only list the devices we care about
+            File[] files = dir.listFiles(new FileFilter() {
+
+                @Override
+                public boolean accept(File pathname) {
+                    //Check if filename is "cpu", followed by a single digit number
+                    if (Pattern.matches("cpu[0-9]", pathname.getName())) {
+                        return true;
+                    }
+                    return false;
+                }
+
+            });
+            //Return the number of cores (virtual CPU devices)
+            return files.length;
+        } catch (Exception e) {
+            //Default to return 1 core
+            return 1;
+        }
+    }
+
+
+    /**
+     * 导入数据库.
+     *
+     * @param context the context
+     * @param dbName  the db name
+     * @param rawRes  the raw res
+     * @return true, if successful
+     */
+    public static boolean importDatabase(Context context, String dbName, int rawRes) {
+        int buffer_size = 1024;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        boolean flag = false;
+
+        try {
+            String dbPath = "/data/data/" + context.getPackageName() + "/databases/" + dbName;
+            File dbfile = new File(dbPath);
+            //判断数据库文件是否存在，若不存在则执行导入，否则直接打开数据库
+            if (!dbfile.exists()) {
+                //欲导入的数据库
+                if (!dbfile.getParentFile().exists()) {
+                    dbfile.getParentFile().mkdirs();
+                }
+                dbfile.createNewFile();
+                is = context.getResources().openRawResource(rawRes);
+                fos = new FileOutputStream(dbfile);
+                byte[] buffer = new byte[buffer_size];
+                int count = 0;
+                while ((count = is.read(buffer)) > 0) {
+                    fos.write(buffer, 0, count);
+                }
+                fos.flush();
+            }
+            flag = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (Exception e) {
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * 获取屏幕尺寸与密度.
+     *
+     * @param context the context
+     * @return mDisplayMetrics
+     */
+    public static DisplayMetrics getDisplayMetrics(Context context) {
+        Resources mResources;
+        if (context == null) {
+            mResources = Resources.getSystem();
+
+        } else {
+            mResources = context.getResources();
+        }
+        //DisplayMetrics{density=1.5, width=480, height=854, scaledDensity=1.5, xdpi=160.421, ydpi=159.497}
+        //DisplayMetrics{density=2.0, width=720, height=1280, scaledDensity=2.0, xdpi=160.42105, ydpi=160.15764}
+        DisplayMetrics mDisplayMetrics = mResources.getDisplayMetrics();
+        return mDisplayMetrics;
+    }
+
+
+    /**
+     * 获取包信息.
+     *
+     * @param context the context
+     */
+    public static PackageInfo getPackageInfo(Context context) {
+        PackageInfo info = null;
+        try {
+            String packageName = context.getPackageName();
+            info = context.getPackageManager().getPackageInfo(packageName, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return info;
+    }
+
+    /**
+     * 获取App版本号
+     *
+     * @param context     上下文
+     * @param packageName 包名
+     * @return App版本号
+     */
+    public static String getAppVersionName(Context context, String packageName) {
+        if (DataUtil.isEmpty(packageName)) return null;
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(packageName, 0);
+            return pi == null ? null : pi.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 获取开发版本号
+     *
+     * @param context
+     * @return
+     */
+    public static String getVersionCode(Context context) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            PackageInfo packInfo = getPackageInfo(context);
+            builder.append(packInfo.versionCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+
+    /**
+     * 是否存在该包名的应用
+     *
+     * @param context
+     * @return
+     */
+    public static boolean exitAppBy(Context context, String packageName) {
+        final PackageManager packageManager = context.getPackageManager();// 获取packagemanager
+        List<PackageInfo> pinfo = packageManager.getInstalledPackages(0);// 获取所有已安装程序的包信息
+        if (pinfo != null) {
+            for (int i = 0; i < pinfo.size(); i++) {
+                String pn = pinfo.get(i).packageName;
+                if (pn.equals(packageName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //获得独一无二的Psuedo ID
+    public static String getUniquePsuedoID() {
+        String serial = null;
+
+        String m_szDevIDShort = "35" +
+                Build.BOARD.length() % 10 + Build.BRAND.length() % 10 +
+
+                Build.CPU_ABI.length() % 10 + Build.DEVICE.length() % 10 +
+
+                Build.DISPLAY.length() % 10 + Build.HOST.length() % 10 +
+
+                Build.ID.length() % 10 + Build.MANUFACTURER.length() % 10 +
+
+                Build.MODEL.length() % 10 + Build.PRODUCT.length() % 10 +
+
+                Build.TAGS.length() % 10 + Build.TYPE.length() % 10 +
+
+                Build.USER.length() % 10; //13 位
+
+        try {
+            serial = Build.class.getField("SERIAL").get(null).toString();
+            //API>=9 使用serial号
+            return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+        } catch (Exception exception) {
+            //serial需要一个初始化
+            serial = "serial"; // 随便一个初始化
+        }
+        //使用硬件信息拼凑出来的15位号码
+        return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+    }
+}
